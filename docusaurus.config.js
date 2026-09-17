47405c3b2a4382a2508f1ef91421cbf7dcca869f
JimKennedy <jim@midokura.com>
2026-09-17 11:23:39 +0200

    fix config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,7 +19,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  // url: 'https://docs.midokura.com',
+  url: 'https://jimken123.github.io/docusaurus',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
